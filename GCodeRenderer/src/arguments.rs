5304022715cd6de9a0a310f5a2061ad237cbd3a5
Austin <ahaskell@carthage.edu>
2020-12-29 22:04:51 -0600

    Added flag and functionality to also create a joined image
    Added -j flag to join all quadrant images into one big image to make it easier to check against the original source.

--- GCodeRenderer/src/arguments.rs
+++ GCodeRenderer/src/arguments.rs
@@ -5,7 +5,8 @@ pub struct ProgramArgs {
     pub input_file: String,
     pub output_file: String,
     pub pixel_size: u32,
-    pub thread_count: u8
+    pub thread_count: u8,
+    pub create_joined: bool 
 }
 
 impl ProgramArgs {
@@ -18,34 +19,45 @@ impl ProgramArgs {
                 .short("f")
                 .long("file")
                 .takes_value(true)
+                .required(true)
                 .help("File to generate images from"))
             .arg(Arg::with_name("output")
                 .short("o")
                 .long("output")
                 .takes_value(true)
+                .required(true)
                 .help("Base output filename, this filename will have the quadrant rendered appended"))
             .arg(Arg::with_name("pixels_per_mm")
                 .short("p")
                 .long("pixel")
                 .takes_value(true)
+                .required(true)
                 .help("Number of pixels to use per mm, default is 1"))
             .arg(Arg::with_name("thread_count")
                 .short("t")
                 .long("threads")
                 .takes_value(true)
+                .required(true)
                 .help("Number of threads to use to speed up rendering"))
+            .arg(Arg::with_name("create_joined_image")
+                .short("j")
+                .long("joined")
+                .takes_value(false)
+                .help("Additionally create joined image that is combined result of all quadrant images"))
             .get_matches();
 
-        let input_file: &str  = run_arguments.value_of("file").unwrap_or("input.gcode");
-        let output_base: &str = run_arguments.value_of("output").unwrap_or("quadrant");
-        let pixel_scalar: u32 = run_arguments.value_of("pixels_per_mm").unwrap_or("1").parse().unwrap_or(1);
-        let thread_count: u8  = run_arguments.value_of("thread_count").unwrap_or("4").parse().unwrap_or(4);
+        let input_file: &str    = run_arguments.value_of("file").unwrap_or("input.gcode");
+        let output_base: &str   = run_arguments.value_of("output").unwrap_or("quadrant");
+        let pixel_scalar: u32   = run_arguments.value_of("pixels_per_mm").unwrap_or("1").parse().unwrap_or(1);
+        let thread_count: u8    = run_arguments.value_of("thread_count").unwrap_or("4").parse().unwrap_or(4);
+        let create_joined: bool = run_arguments.is_present("create_joined_image");
 
         ProgramArgs {
             input_file: String::from(input_file),
             output_file: String::from(output_base),
             pixel_size: pixel_scalar,
-            thread_count: thread_count
+            thread_count: thread_count,
+            create_joined: create_joined
         }
     }
 }
--- GCodeRenderer/src/main.rs
+++ GCodeRenderer/src/main.rs
@@ -11,6 +11,8 @@ use png::*;
 use indicatif::ProgressBar;
 use indicatif::ProgressStyle;
 use threadpool::ThreadPool;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::mpsc;
 
 mod file_loader;
 use file_loader::*;
@@ -20,6 +22,11 @@ use rasterizer::*;
 
 mod arguments;
 use arguments::*;
+
+struct RenderedQuadrant {
+    id: (i32, i32),
+    image_data: Vec<u8>
+}
 
 fn main() {
     
@@ -50,7 +57,6 @@ fn main() {
     let progress_bar = ProgressBar::new(loader.commands.len() as u64);
     progress_bar.set_style(ProgressStyle::default_bar().progress_chars("#>-"));
 
-    //TODO: Make this multithreaded. - Austin Haskell
     let printbed_width  = loader.settings.printbed_width as i32;
     let printbed_height = loader.settings.printbed_height as i32;
     let pixel_scaling = program_args.pixel_size as i32;
@@ -58,9 +64,11 @@ fn main() {
     let workers = threadpool::ThreadPool::new(program_args.thread_count as usize);
 
     let command_len = loader.commands.len();
+    let (tx, rx): (Sender<RenderedQuadrant>, Receiver<RenderedQuadrant>) = mpsc::channel();
     for command in loader.commands {
         let output_base = program_args.output_file.clone();
 
+        let transmitter = tx.clone();
         workers.execute( move || {
             let img = Rasterizer::create(
                 &command, 
@@ -68,6 +76,11 @@ fn main() {
                 printbed_height, 
                 pixel_scaling);
     
+            let quad_data = RenderedQuadrant {
+                id: (command.quadrant.x, command.quadrant.y),
+                image_data: img.data.clone()
+            };
+            
             let mut filename_builder = string_builder::Builder::default();
             filename_builder.append(output_base);
             filename_builder.append(command.quadrant.x.to_string());
@@ -75,29 +88,108 @@ fn main() {
             filename_builder.append(command.quadrant.y.to_string());
             filename_builder.append(".png");
 
-            let filename = filename_builder.string().unwrap();
-            let file = File::create(Path::new(&filename)).unwrap();
-            let ref mut buffered_writer = BufWriter::new(file);
-    
-            let mut encoder = png::Encoder::new(buffered_writer, img.width as u32, img.height as u32);
-            encoder.set_color(png::ColorType::RGB);
-            encoder.set_depth(png::BitDepth::Eight);
-            let mut writer = encoder.write_header().unwrap();
-            
-            writer.write_image_data(&img.data).unwrap(); // Save
+            write_png(&filename_builder.string().unwrap(), img.width, img.height, img.data).unwrap();
+
+            if transmitter.send(quad_data).is_err() {}
         });
     }
 
+    let mut master_image_data: Vec<RenderedQuadrant> = Vec::new();
     // Note: I dont know if this could break. As in, I dont know if theres a scenario 
     //  where the active workers is 0 but there is still queued jobs. - Austin Haskell
-    while workers.active_count() != 0 {
-        progress_bar.set_position((command_len - workers.queued_count()) as u64);
+    while master_image_data.len() != command_len {
+
+        let master_img_quadrant = rx.try_recv();
+        if master_img_quadrant.is_ok() {
+            master_image_data.push(master_img_quadrant.unwrap());
+        }
+
+        progress_bar.set_position(master_image_data.len() as u64);
     }
 
     workers.join();
-
     progress_bar.finish();
+
+    if program_args.create_joined {
+        println!("Creating master image from quadrants... ");
+        create_joined_image(master_image_data, printbed_width, printbed_height, program_args.pixel_size as i32);
+        println!("Done.");
+    }
 
     println!("Completed rendering. ");
 
+}
+
+fn create_joined_image(rendered_quadrants: Vec<RenderedQuadrant>, quadrant_img_width: i32, quadrant_img_height: i32, scale: i32) {
+    
+    let mut largest_quadrants: (i32, i32) = (0, 0);
+    for quadrant in &rendered_quadrants {
+        if quadrant.id.0 > largest_quadrants.0 {
+            largest_quadrants.0 = quadrant.id.0;
+        }
+        if quadrant.id.1 > largest_quadrants.1 {
+            largest_quadrants.1 = quadrant.id.1;
+        }
+    }
+
+    let image_width: usize  = ((largest_quadrants.0 + 1) * quadrant_img_width  * scale) as usize;
+    let image_height: usize = ((largest_quadrants.1 + 1) * quadrant_img_height * scale) as usize;
+    let mut master_image: Vec<u8> = vec![100; image_height * image_width * 3];
+
+    // TODO: Clean this up, it's pretty unreadable. 
+    // The problem I was trying to solve was mapping each of the image data chunks to the master image's 
+    //  data. They're both single dimentional array's representing two dimentional data so there's a lot 
+    //  of offsets and shifting going on. Also the quadrant 0,0 goes in the bottom left and not the top 
+    //  left, so there's also a flip of the quadrants y value. 
+    for quadrant in rendered_quadrants {
+        let quadrant_width = quadrant_img_width * scale * 3;
+        let quadrant_height = quadrant_img_width * scale;
+
+        let flipped_y = (quadrant.id.1 - largest_quadrants.1).abs();
+        let mut line_pre = flipped_y as usize * image_width * 3 * quadrant_height as usize 
+            + quadrant.id.0 as usize * quadrant_width as usize; 
+
+        for y in 0..quadrant_height {
+            for x in 0..quadrant_width {
+                let pixel_coord: usize = (y * quadrant_width + x) as usize; 
+
+                master_image[line_pre + x as usize] = quadrant.image_data[pixel_coord];
+            }
+            line_pre += image_width * 3;
+        }
+    }
+
+    write_png("master_img.png", 
+        image_width,
+        image_height,
+        master_image
+    ).unwrap();
+}
+
+
+// TODO: Figure out if the library is causing the slowdown. Seems to me like the write takes wayyyy too long for how much I'm writing.
+//  maybe use the native library calls and make the header myself? - Austin Haskell
+fn write_png(filename: &str, width: usize, height: usize, img_data: Vec<u8>) -> Result<(), &str> {
+    let file = File::create(Path::new(filename));
+    if file.is_err() {
+        return Err("Failed to create file. Is the path valid?");
+    }
+
+    let ref mut buffered_writer = BufWriter::new(file.unwrap());
+
+    let mut encoder = png::Encoder::new(buffered_writer, width as u32, height as u32);
+    encoder.set_color(png::ColorType::RGB);
+    encoder.set_depth(png::BitDepth::Eight);
+
+    let writer = encoder.write_header();
+    if writer.is_err() {
+        return Err("Failed to encode PNG header. ");
+    }
+    
+    match writer.unwrap().write_image_data(&img_data) {
+        Err(e) => println!("{:?}", e),
+        _ => {},
+    }
+
+    Ok(())
 }
